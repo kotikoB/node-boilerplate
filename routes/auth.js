3fd7366c2be4f11ce8dca655bcef2dac03598651
Stephen Ouma <stephen.ouma@Softwaregroup-bg.com>
2019-11-19 12:29:45 +0300

    prettier settings doublequotes

--- routes/auth.js
+++ routes/auth.js
@@ -1,51 +1,51 @@
-const router = require('express').Router();
-const User = require('../model/User');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const { registerValidation, loginValidation } = require('../validation');
+const router = require("express").Router();
+const User = require("../model/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { registerValidation, loginValidation } = require("../validation");
 
-router.post('/register', async (req, res) => {
-    const { error } = registerValidation(req.body);
-    if (error) return res.status(400).send(error.details);
+router.post("/register", async (req, res) => {
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details);
 
-    // check if user exists
-    const userExists = await User.findOne({ email: req.body.email });
-    if (userExists) return res.status(400).send('User already exists in the database');
+  // check if user exists
+  const userExists = await User.findOne({ email: req.body.email });
+  if (userExists) return res.status(400).send("User already exists in the database");
 
-    // hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    // create new user
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword
-    });
-    try {
-        const savedUser = await user.save();
-        res.send(savedUser);
-    } catch (err) {
-        console.log(err);
-        res.status(400).send(err);
-    }
+  // create new user
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword
+  });
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err);
+  }
 });
 
-router.post('/login', async (req, res) => {
-    const { error } = loginValidation(req.body);
-    if (error) return res.status(400).send(error.details);
+router.post("/login", async (req, res) => {
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details);
 
-    // check if user exists
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(400).send(`Sorry that email doesn't exist`);
+  // check if user exists
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send(`Sorry that email doesn't exist`);
 
-    // verify password
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(400).send('The password is invalid');
+  // verify password
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("The password is invalid");
 
-    // assign token
-    const token = jwt.sign({ _id: user._id, name: user.name }, process.env.TOKEN_SECRET);
-    res.header('token', token).send(token);
+  // assign token
+  const token = jwt.sign({ _id: user._id, name: user.name }, process.env.TOKEN_SECRET);
+  res.header("token", token).send(token);
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
-const verify = require('./verifytoken');
+const router = require("express").Router();
+const verify = require("./verifytoken");
 
-router.get('/', verify, (req, res) => {
-    // Access to the user from request is enabled by the verified token
-    res.status(200).send({ user: req.user, msg: 'These are the users' });
+router.get("/", verify, (req, res) => {
+  // Access to the user from request is enabled by the verified token
+  res.status(200).send({ user: req.user, msg: "These are the users" });
 });
 
 module.exports = router;
